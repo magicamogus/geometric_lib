cb77247b8adc10c617bd336f85a1c97379ee5529
Ksenia Ivanova <sweetbananyauwu@gmail.com>
2024-02-17 21:12:04 +0300

    Add area and perimeter coments to square.py

--- square.py
+++ square.py
@@ -1,5 +1,23 @@
 def area(a):
+    """
+    Calculate the area of a square.
+
+    Parameters:
+    a (float): The length of one side of the square.
+
+    Returns:
+    float: The area of the square.
+    """
     return a * a
 
 def perimeter(a):
+    """
+    Calculates the perimeter of a square.
+
+    Parameters:
+    a (float): The length of one side of the square.
+
+    Returns:
+    float: The perimeter of the square.
+    """
     return 4 * a
